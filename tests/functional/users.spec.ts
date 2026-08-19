7e8158a84f786c714ae1dfe8d69a4de24e2761bd
ryanmarinhodev <ryanmarinhodev@gmail.com>
2026-08-19 08:20:47 -0300

    fix: altering variable name

--- tests/functional/users.spec.ts
+++ tests/functional/users.spec.ts
@@ -5,19 +5,19 @@ const baseUrl = `http://${process.env.HOST}:${process.env.PORT}`
 
 test.group('User', () => {
   test('It should create an user', async ({ assert }) => {
-    const payload = {
+    const requestData = {
       name: 'teste4',
       email: 'test@test4',
       password: 'teste123',
     }
 
     // const { body } = await superTest(baseUrl).post('/users').send(payload).expect(201)
-    const response = await superTest(baseUrl).post('/users').send(payload)
+    const response = await superTest(baseUrl).post('/users').send(requestData)
 
     assert.exists(response.body.user, 'User Undefined')
     assert.exists(response.body.user.id, 'Id Undefined')
-    assert.equal(response.body.user.name, payload.name)
-    assert.equal(response.body.user.email, payload.email)
-    assert.equal(response.body.user.password, payload.password)
+    assert.equal(response.body.user.name, requestData.name)
+    assert.equal(response.body.user.email, requestData.email)
+    assert.equal(response.body.user.password, requestData.password)
   }).pin()
 })
