13fcb3a72c7621c9e2d681ad271fd91f792a31d2
ryanmarinhodev <ryanmarinhodev@gmail.com>
2026-08-24 10:47:50 -0300

    wip: add hash password

--- tests/functional/users.spec.ts
+++ tests/functional/users.spec.ts
@@ -1,23 +1,27 @@
 import { test } from '@japa/runner'
 import superTest from 'supertest'
+import Hash from '@ioc:Adonis/Core/Hash'
 
 const baseUrl = `http://${process.env.HOST}:${process.env.PORT}`
 
 test.group('User', () => {
   test('It should create an user', async ({ assert }) => {
     const requestData = {
-      name: 'teste4',
-      email: 'test@test4',
+      name: 'testehash3',
+      email: 'testehash3@teste.com',
       password: 'teste123',
     }
 
+    console.log(requestData)
+
     // const { body } = await superTest(baseUrl).post('/users').send(payload).expect(201)
     const response = await superTest(baseUrl).post('/users').send(requestData)
+    const hashedPassword = await Hash.make(response.body.user.password)
 
     assert.exists(response.body.user, 'User Undefined')
     assert.exists(response.body.user.id, 'Id Undefined')
     assert.equal(response.body.user.name, requestData.name)
     assert.equal(response.body.user.email, requestData.email)
-    assert.equal(response.body.user.password, requestData.password)
+    assert.notEqual(hashedPassword, 'Indefinida')
   }).pin()
 })
